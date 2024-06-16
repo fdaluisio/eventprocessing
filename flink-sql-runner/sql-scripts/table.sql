baad4d8cbde047fa2c75ca8ee676d68e55e2f223
Francesco D'Aluisio <francesco.daluisio@ssbprogetti.it>
2024-06-16 13:23:14 +0200

    fix auth scam for flink in kafka cluster

--- flink-sql-runner/sql-scripts/table.sql
+++ flink-sql-runner/sql-scripts/table.sql
@@ -1,32 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 CREATE TABLE orders (
-  order_number BIGINT,
-  price        DECIMAL(32,2),
-  buyer        ROW<first_name STRING, last_name STRING>,
-  order_time   TIMESTAMP(3)
+  order_number INT,
+  price        INT
 ) WITH (
-  'connector' = 'datagen'
+  'connector' = 'datagen',
+  'rows-per-second' = '1',
+  'fields.order_number.kind' = 'sequence',
+  'fields.order_number.start' = '1',
+  'fields.order_number.end' = '1000000',
+  'fields.price.min' = '1000',
+  'fields.price.max' = '1500'
 );
 
-CREATE TABLE print_table WITH ('connector' = 'print')
-  LIKE orders;
+CREATE TABLE KafkaTable (
+  `order_number` INT,
+  `price` INT
+) WITH (
+  'connector' = 'kafka',
+  'topic' = 'orders',
+  'properties.bootstrap.servers' = 'svil-es-kafka-kafka-bootstrap.kafka.svc:9092',
+  'properties.group.id' = 'testGroup',
+  'scan.startup.mode' = 'earliest-offset',
+  'format' = 'json',
+  'json.ignore-parse-errors' = 'true'
+  'properties.sasl.mechanism' = 'SCRAM-SHA-512',
+  'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.scram.ScramLoginModule required username="fd-kafkaUser" password="Wnb0mGgaGLkFIc49hvARCyyFRA0ojwNd";'
+);
 
-INSERT INTO print_table SELECT * FROM orders;
+INSERT INTO KafkaTable SELECT * FROM orders;
