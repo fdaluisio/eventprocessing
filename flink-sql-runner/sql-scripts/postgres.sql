f1a196ca4616f7507e59f1870da983eca3f0cce3
Francesco D'Aluisio <francesco.daluisio@ssbprogetti.it>
2024-06-16 17:45:30 +0200

    add postgres sink

--- flink-sql-runner/sql-scripts/postgres.sql
+++ flink-sql-runner/sql-scripts/postgres.sql
@@ -1,19 +1,6 @@
-CREATE TABLE orders (
-  order_number INT,
-  price        INT
-) WITH (
-  'connector' = 'datagen',
-  'rows-per-second' = '1',
-  'fields.order_number.kind' = 'sequence',
-  'fields.order_number.start' = '1',
-  'fields.order_number.end' = '1000000',
-  'fields.price.min' = '1000',
-  'fields.price.max' = '1500'
-);
-
 CREATE TABLE KafkaTable (
-  `order_number` BIGINT,
-  `price` BIGINT
+  `order_number` INT,
+  `price` INT
 ) WITH (
   'connector' = 'kafka',
   'topic' = 'orders',
@@ -21,9 +8,21 @@ CREATE TABLE KafkaTable (
   'properties.group.id' = 'ordersGroup',
   'scan.startup.mode' = 'earliest-offset',
   'format' = 'json',
-  'json.ignore-parse-errors' = 'true',
+  'json.ignore-parse-errors' = 'true'
   'properties.sasl.mechanism' = 'SCRAM-SHA-512',
   'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.scram.ScramLoginModule required username="fd-kafkauser" password="Wnb0mGgaGLkFIc49hvARCyyFRA0ojwNd";'
 );
 
-INSERT INTO KafkaTable SELECT * FROM orders;
+CREATE TABLE OrdersTable (
+  order_number INT,
+  price INT
+) WITH (
+  'connector' = 'jdbc',
+  'url' = 'jdbc:postgresql://postgres.flink-test.svc:5432/OrdersTable',
+  'username' = 'franco',
+  'password' = 'franco',
+  'table-name' = 'OrdersTable'
+);
+
+INSERT INTO OrdersTable SELECT * FROM KafkaTable;
+
